d52a9f905532a69432b8bb9de75561a6e6174175
Derek Li <derekl52738@gmail.com>
2022-04-16 21:45:44 -0400

    running routes is hard

--- index.js
+++ index.js
@@ -38,6 +38,74 @@ function getUserLocation() {
         var pin = new Microsoft.Maps.Pushpin(loc);
         recentPin=pin;
         map.entities.push(pin);
-        map.setView({center: loc, zoom: 13})
+        map.setView({center: loc, zoom: 13});
+        coords=String(position.coords.latitude)+", "+String(position.coords.longitude);
+        var url="https://dev.virtualearth.net/REST/v1/Locations/"+coords+"?includeEntityTypes=&o=json&key=Am8ryQPYHCOfmbeh9Uk6tulI8N1xFN-Ab9DRLdmRbDtaJ8SFj1U00pU5LW9xzzac";
+        var name;
+        fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            for(const tempList of out["resourceSets"][0]["resources"]){
+                if((typeof name)=="undefined"){
+                    if("confidence" in tempList){
+                        if(tempList["confidence"]!="Low"){
+                            if("name" in tempList){
+                                name=tempList["name"];
+                            }
+                        }
+                    }
+                }
+            }
+            if((typeof name)=="undefined"){
+                document.getElementById("searchBox").value=coords;
+            }
+            else{
+                document.getElementById("searchBox").value=name;
+            }
+        })
+        .catch(err => { 
+            par.innerHTML="Please enter in a valid address";
+            throw err 
+        });
+    });
+}
+
+function findRoute() {
+    var par = document.getElementById("routeResult");
+    var distance = document.getElementById("distance").value;
+    var loc = document.getElementById("searchBox").value;
+    if(document.getElementById("unit").value==="mi"){
+        distance=distance*1.60934;
+    }
+    var url="https://dev.virtualearth.net/REST/v1/Locations/"+loc+"?includeEntityTypes=&o=json&key=Am8ryQPYHCOfmbeh9Uk6tulI8N1xFN-Ab9DRLdmRbDtaJ8SFj1U00pU5LW9xzzac";
+    var lat, long, coords;
+    fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+        for(const tempList of out["resourceSets"][0]["resources"]){
+            if((typeof lat)=="undefined"){
+                if("confidence" in tempList){
+                    if(tempList["confidence"]!="Low"){
+                        if("point" in tempList){
+                            if("coordinates" in tempList["point"]){
+                                lat=tempList["point"]["coordinates"][0];
+                                long=tempList["point"]["coordinates"][1];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        coords=lat+", "+long;
+        if((typeof lat)=="undefined"){
+            par.innerHTML="Please enter in a valid address"
+        }
+        else{
+            par.innerHTML=coords;
+        }
+    })
+    .catch(err => { 
+        par.innerHTML="Please enter in a valid address";
+        throw err 
     });
 }
